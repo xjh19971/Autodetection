76169450ece4cfa48fd3a6b26ddd5f938aee9244
xjh <414581775@qq.com>
2020-05-02 13:39:09 -0400

    softmax LR AUC

--- utils/helper.py
+++ utils/helper.py
@@ -4,8 +4,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
-
-batch_size=4
+from shapely.geometry import Polygon
+step_size=4
 def convert_map_to_lane_map(ego_map, binary_lane):
     mask = (ego_map[0,:,:] == ego_map[1,:,:]) * (ego_map[1,:,:] == ego_map[2,:,:]) + (ego_map[0,:,:] == 250 / 255)
 
@@ -37,13 +37,13 @@ def collate_fn(batch):
     concated_roadmap=[]
     for i in range(len(batch)):
         concated_roadmap.append(batch[i][2].unsqueeze(0))
-    concated_roadmap=torch.cat(concated_roadmap,0).unsqueeze(1).float()
+    concated_roadmap=torch.cat(concated_roadmap,0).long()
     return [concated_input,bbox_list,category_list,concated_roadmap]
 
 def collate_fn_lstm(batch):
     concated_input=[]
     for i in range(len(batch)):
-        concated_input.append(batch[i][0].view(batch_size,18,128,160))
+        concated_input.append(batch[i][0].view(step_size,18,128,160))
     concated_input=torch.stack(concated_input)
     batch_bbox_list=[]
     batch_category_list=[]
@@ -63,8 +63,8 @@ def collate_fn_lstm(batch):
         batch_category_list.append(category_list)
     concated_roadmap=[]
     for i in range(len(batch)):
-        concated_roadmap.append(batch[i][2].unsqueeze(1))
-    concated_roadmap=torch.stack(concated_roadmap).float()
+        concated_roadmap.append(batch[i][2])
+    concated_roadmap=torch.stack(concated_roadmap).long()
     return [concated_input,batch_bbox_list,batch_category_list,concated_roadmap]
 
 def collate_fn_unlabeled(batch):
@@ -83,6 +83,17 @@ def draw_box(ax, corners, color):
     ax.plot(point_squence.T[0] * 10 + 400, -point_squence.T[1] * 10 + 400, color=color)
 
 
+def compute_ts_road_map(road_map1, road_map2):
+    tp = (road_map1 * road_map2).sum()
+
+    return tp * 1.0 / (road_map1.sum() + road_map2.sum() - tp)
+
+
+def compute_iou(box1, box2):
+    a = Polygon(torch.t(box1)).convex_hull
+    b = Polygon(torch.t(box2)).convex_hull
+
+    return a.intersection(b).area / a.union(b).area
 
 
 
