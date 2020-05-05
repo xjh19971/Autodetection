02135994b8fe3595ec6f69445980b10ed4d666bd
xjh <414581775@qq.com>
2020-05-05 06:25:20 -0400

    try resize

--- model/pretrainModel.py
+++ model/pretrainModel.py
@@ -108,7 +108,7 @@ class AutoPretrainNet(nn.Module):
 
     def forward(self, x):
         batch_size = x.size(0)
-        x = x.view(x.size(0) * 6, -1, 128, 160)
+        x = x.view(x.size(0) * 6, -1, 256, 320)
         x = self.efficientNet(x,pretrain=True)
         x = x.view(x.size(0), 2, -1)
         mu = x[:, 0, :]
