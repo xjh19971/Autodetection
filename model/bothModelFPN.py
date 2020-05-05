4e80b4c59c7db2d06d79261f5f4d679be2c0b4a6
xjh <414581775@qq.com>
2020-05-05 03:05:24 -0400

    try FPN

--- model/bothModelFPN.py
+++ model/bothModelFPN.py
@@ -47,8 +47,8 @@ class BasicBlock(nn.Module):
 class AutoNet(nn.Module):
     def __init__(self, scene_batch_size, batch_size, step_size, device, anchors, detection_classes, num_classes=2):
         self.latent = 1000
-        self.fc_num1 = 300
-        self.fc_num2 = 200
+        self.fc_num1 = 400
+        self.fc_num2 = 400
         self.batch_size = batch_size
         self.step_size = step_size
         self.scene_batch_size = scene_batch_size
