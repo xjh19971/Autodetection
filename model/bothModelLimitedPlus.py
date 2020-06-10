fe82d394699fa05a9351a8a69a77c4875a910cfc
xjh <414581775@qq.com>
2020-06-10 01:42:18 -0400

    self learning (loss mask incomplete)

--- model/bothModelLimitedPlus.py
+++ model/bothModelLimitedPlus.py
@@ -14,8 +14,8 @@ from utils.yolo_utils import get_anchors
 class AutoNet(pl.LightningModule):
     def __init__(self, hparams):
         super().__init__()
-        self.fc_num1 = 400
-        self.fc_num2 = 600
+        self.fc_num1 = 300
+        self.fc_num2 = 500
         self.hparams = hparams
         self.learning_rate = hparams.learning_rate
         self.anchors = get_anchors(hparams.anchors_file)
@@ -68,18 +68,53 @@ class AutoNet(pl.LightningModule):
         for i in range(6):
             if i != 1 and i != 4:
                 self.fc2_1.append(nn.Sequential(
-                    nn.Linear(self.fc_num2, 14 * 13 * 64, bias=False),
+                    nn.Linear(self.fc_num2, 14 * 13 * 32, bias=False),
                     nn.BatchNorm1d(14 * 13 * 32),
                     nn.ReLU(inplace=True),
                     nn.Dropout(0.2),
                 ))
             else:
                 self.fc2_1.append(nn.Sequential(
-                    nn.Linear(self.fc_num2, 13 * 18 * 64, bias=False),
+                    nn.Linear(self.fc_num2, 13 * 18 * 32, bias=False),
                     nn.BatchNorm1d(13 * 18 * 32),
                     nn.ReLU(inplace=True),
                     nn.Dropout(0.2),
                 ))
+        self.fc1_1 = nn.Sequential(
+            nn.Linear(32 * 4 * 5, self.fc_num2, bias=False),
+            nn.BatchNorm1d(self.fc_num2),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.2),
+        )
+        self.reversedfc2 = nn.ModuleList([])
+        for i in range(6):
+            if i != 1 and i != 4:
+                self.reversedfc2.append(nn.Sequential(
+                    nn.Linear(14 * 13 * 64, self.fc_num1 + self.fc_num2, bias=False),
+                    nn.BatchNorm1d(self.fc_num1 + self.fc_num2),
+                    nn.ReLU(inplace=True),
+                    nn.Dropout(0.2),
+                ))
+            else:
+                self.reversedfc2.append(nn.Sequential(
+                    nn.Linear(13 * 18 * 64, self.fc_num1 + self.fc_num2, bias=False),
+                    nn.BatchNorm1d(self.fc_num1 + self.fc_num2),
+                    nn.ReLU(inplace=True),
+                    nn.Dropout(0.2),
+                ))
+        self.reversedfc1 = nn.Sequential(
+            nn.Linear(self.fc_num1 + self.fc_num2, 64 * 4 * 5, bias=False),
+            nn.BatchNorm1d(64 * 4 * 5),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.2),
+        )
+        self.recompressed = nn.Sequential(
+            nn.Conv2d(64, 256, 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.2),
+        )
+
         self.inplanes = 32
         self.conv0 = self._make_layer(BasicBlock, 32, 2)
         self.deconv0 = self._make_deconv_layer(32, 16)
@@ -94,18 +129,31 @@ class AutoNet(pl.LightningModule):
         self.deconv3 = self._make_deconv_layer(4, 2)
         self.convfinal = nn.Conv2d(2, 2, 1)
 
-        self.inplanes = 64
-        self.conv0_1_detect = self._make_layer(BasicBlock, 64, 2)
-        self.convfinal_0 = nn.Conv2d(64, len(self.anchors0) * (self.detection_classes + 5), 1)
+        self.inplanes = 32
+        self.conv0_1_detect = self._make_layer(BasicBlock, 32, 2)
+        self.convfinal_0 = nn.Conv2d(32, len(self.anchors0) * (self.detection_classes + 5), 1)
         self.yolo0 = YOLOLayer(self.anchors0, self.detection_classes, 800)
-        self.conv0_1 = self._make_layer(BasicBlock, 64, 2)
-        self.deconv0_1 = self._make_deconv_layer(64, 16)
+        self.conv0_1 = self._make_layer(BasicBlock, 32, 2)
+        self.deconv0_1 = self._make_deconv_layer(32, 16)
 
         self.inplanes = 16
         self.conv1_1_detect = self._make_layer(BasicBlock, 16, 2)
         self.convfinal_1 = nn.Conv2d(16, len(self.anchors1) * (self.detection_classes + 5), 1)
         self.yolo1 = YOLOLayer(self.anchors1, self.detection_classes, 800)
+        self.conv1_1 = self._make_layer(BasicBlock, 16, 2)
 
+        self.deconv0_for_self = self._make_deconv_layer(256, 128)
+        self.inplanes = 128
+        self.conv0_for_self = self._make_layer(BasicBlock, 128, 2)
+        self.deconv1_for_self = self._make_deconv_layer(128, 64)
+        self.inplanes = 64
+        self.conv1_for_self = self._make_layer(BasicBlock, 64, 2)
+        self.deconv2_for_self = self._make_deconv_layer(64, 32)
+        self.inplanes = 32
+        self.conv2_for_self = self._make_layer(BasicBlock, 32, 2)
+        self.deconv3_for_self = self._make_deconv_layer(32, 16)
+        self.conv3_for_self = nn.Conv2d(16, 3, 1)
+        self.upSample = nn.Upsample(scale_factor=2)
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
@@ -150,7 +198,28 @@ class AutoNet(pl.LightningModule):
                 output[:, :, 12:, :14] += block(x[:, i, :]).view(x.size(0), -1, 13, 14)
         return output
 
+    def ReversedlimitedFC1(self, x, fc):
+        output = []
+        for i, block in enumerate(fc):
+            if i == 0:
+                output.append(block(x[:, :, :13, 11:].reshape(x.size(0), -1)))
+            elif i == 1:
+                output.append(block(x[:, :, 4:22, 12:].reshape(x.size(0), -1)))
+            elif i == 2:
+                output.append(block(x[:, :, 12:, 11:].reshape(x.size(0), -1)))
+            elif i == 3:
+                output.append(block(x[:, :, :13, :14].reshape(x.size(0), -1)))
+            elif i == 4:
+                output.append(block(x[:, :, 4:22, :13].reshape(x.size(0), -1)))
+            elif i == 5:
+                output.append(block(x[:, :, 12:, :14].reshape(x.size(0), -1)))
+        output = torch.stack(output, dim=1)
+        output = output.view(output.size(0) * 6, -1)
+        return output
+
     def forward(self, x, detection_target=None):
+        scene= x.size(0)
+        batch= x.size(1)
         x = x.view(-1, 3, 128, 160)
         output_list = self.efficientNet(x)
         x = output_list[2]
@@ -158,8 +227,8 @@ class AutoNet(pl.LightningModule):
         x1 = self.compressed(x)
         x1 = x1.view(x1.size(0), -1)
         x1 = self.fc1(x1)
-        x1 = self.limitedFC1(x1, self.fc2, 32)
-        x1 = self.conv0(x1)
+        x1_for_x3 = self.limitedFC1(x1, self.fc2, 32)
+        x1 = self.conv0(x1_for_x3)
         x1 = self.deconv0(x1)
         x1 = self.conv1(x1)
         x1 = self.deconv1(x1)
@@ -172,8 +241,8 @@ class AutoNet(pl.LightningModule):
         x2 = self.compressed_1(x)
         x2 = x2.view(x2.size(0), -1)
         x2 = self.fc1_1(x2)
-        x2 = self.limitedFC1(x2, self.fc2_1, 32)
-        x2 = self.conv0_1(x2)
+        x2_for_x3 = self.limitedFC1(x2, self.fc2_1, 32)
+        x2 = self.conv0_1(x2_for_x3)
         detect_output0 = self.conv0_1_detect(x2)
         detect_output0 = self.convfinal_0(detect_output0)
         detect_output0, detect_loss0 = self.yolo0(detect_output0, detection_target, 800)
@@ -184,19 +253,35 @@ class AutoNet(pl.LightningModule):
         detect_output1 = self.convfinal_1(detect_output1)
         detect_output1, detect_loss1 = self.yolo1(detect_output1, detection_target, 800)
         total_loss = 0.6 * detect_loss0 + 0.4 * detect_loss1
-        return nn.LogSoftmax(dim=1)(x1), detect_output0, detect_output1, total_loss
 
-    def loss_function(self, outputs, road_image):
+        x3 = torch.cat([x1_for_x3, x2_for_x3], dim=1)
+        x3 = self.ReversedlimitedFC1(x3, self.reversedfc2)
+        x3 = self.reversedfc1(x3)
+        x3 = x3.view(x.size(0), -1, 4, 5)
+        x3 = self.recompressed(x3)
+        x3 = self.deconv0_for_self(x3)
+        x3 = self.conv0_for_self(x3)
+        x3 = self.deconv1_for_self(x3)
+        x3 = self.conv1_for_self(x3)
+        x3 = self.deconv2_for_self(x3)
+        x3 = self.conv2_for_self(x3)
+        x3 = self.deconv3_for_self(x3)
+        x3 = self.conv3_for_self(x3)
+        x3 = self.upSample(x3)
+        x3 = x3.view(scene,batch,18,128,160)
+        return nn.LogSoftmax(dim=1)(x1), detect_output0, detect_output1, total_loss, nn.Sigmoid()(x3)
+
+    def loss_function(self, outputs, road_image, image):
         road_loss = nn.NLLLoss()(outputs[0], road_image)
         detection_loss = outputs[3]
-        loss = road_loss + detection_loss
-        return loss
+        self_loss = nn.BCELoss()(outputs[4], image)
+        loss = road_loss + detection_loss + self_loss
+        return loss, self_loss
 
     def training_step(self, batch, batch_idx):
-        sample, bbox_list, category_list, road_image = batch
-        sample, road_image = sample, road_image
+        sample, bbox_list, category_list, road_image, loss_mask = batch
         outputs = self(sample, [bbox_list, category_list])
-        loss = self.loss_function(outputs, road_image)
+        loss, self_loss = self.loss_function(outputs, road_image, sample)
         output0 = outputs[0].view(-1, 2, 400, 400)
         road_image = road_image.view(-1, 400, 400)
         _, predicted = torch.max(output0.data, 1)
@@ -204,15 +289,15 @@ class AutoNet(pl.LightningModule):
         P = torch.tensor((self.yolo0.metrics['precision'] + self.yolo1.metrics['precision']) / 2)
         R = torch.tensor((self.yolo0.metrics['recall50'] + self.yolo1.metrics['recall50']) / 2)
         lr = torch.tensor(self.trainer.optimizers[0].param_groups[0]['lr'])
-        log_bar = {'roadmap_score': AC, 'precision': P, 'recall': R, 'lr': lr}
-        log = {'loss': loss, 'roadmap_score': AC, 'precision': P, 'recall': R, 'lr': lr}
+        log_bar = {'roadmap_score': AC, 'precision': P, 'recall': R, 'self_loss': self_loss, 'lr': lr}
+        log = {'loss': loss, 'roadmap_score': AC, 'precision': P, 'recall': R, 'self_loss': self_loss, 'lr': lr}
         return {'loss': loss, 'log': log, 'progress_bar': log_bar}
 
     def validation_step(self, batch, batch_idx):
         sample, bbox_list, category_list, road_image = batch
         sample, road_image = sample, road_image
         outputs = self(sample, [bbox_list, category_list])
-        loss = self.loss_function(outputs, road_image)
+        loss, self_loss = self.loss_function(outputs, road_image, sample)
         # Pass data through model
         outputs = self(sample, [bbox_list, category_list])
         output0 = outputs[0].view(-1, 2, 400, 400)
@@ -221,14 +306,16 @@ class AutoNet(pl.LightningModule):
         AC = compute_ts_road_map(predicted, road_image)
         P = torch.tensor((self.yolo0.metrics['precision'] + self.yolo1.metrics['precision']) / 2)
         R = torch.tensor((self.yolo0.metrics['recall50'] + self.yolo1.metrics['recall50']) / 2)
-        return {'val_loss': loss, 'roadmap_score': AC, 'precision': P, 'recall': R}
+        return {'val_loss': loss, 'roadmap_score': AC, 'precision': P, 'recall': R, 'self_loss': self_loss}
 
     def validation_epoch_end(self, outputs):
         avg_val_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
         avg_AC = torch.stack([x['roadmap_score'] for x in outputs]).mean()
         avg_P = torch.stack([x['precision'] for x in outputs]).mean()
         avg_R = torch.stack([x['recall'] for x in outputs]).mean()
-        log = {'avg_val_loss': avg_val_loss, 'avg_roadmap_score': avg_AC, 'avg_precision': avg_P, 'avg_recall': avg_R}
+        avg_self_loss = torch.stack([x['self_loss'] for x in outputs]).mean()
+        log = {'avg_val_loss': avg_val_loss, 'avg_roadmap_score': avg_AC, 'avg_precision': avg_P, 'avg_recall': avg_R,
+               'avg_self_loss': avg_self_loss}
         return {'log': log, 'val_loss': avg_val_loss}
 
     def configure_optimizers(self):
---
+++ training/trainBothSelf.py
@@ -0,0 +1,61 @@
+from argparse import ArgumentParser
+
+import numpy as np
+import pytorch_lightning as pl
+import torch
+from torchvision import transforms
+
+import model.bothModelLimitedPlus as bothModel
+from dataset.dataHelper import UnLabeledPlusLabeledDatasetScene
+from utils.helper import collate_fn_lstm
+# All the images are saved in image_folder
+# All the labels are saved in the annotation_csv file
+
+image_folder = 'dataset/data'
+annotation_csv = 'dataset/data/annotation.csv'
+# You shouldn't change the unlabeled_scene_index
+# The first 106 scenes are unlabeled
+unlabeled_scene_index = np.arange(106)
+# The scenes from 106 - 133 are labeled
+# You should devide the labeled_scene_index into two subsets (training and validation)
+labeled_scene_index = np.arange(106, 134)
+pretrain_file = None
+
+if __name__ == '__main__':
+    parser1 = ArgumentParser()
+    trainparser = pl.Trainer.add_argparse_args(parser1)
+    trainparser.add_argument('--batch_size',type=int, default=8)
+    trainparser.set_defaults(gpus=1)
+    trainparser.set_defaults(max_epochs=3000)
+    trainparser = bothModel.AutoNet.add_model_specific_args(trainparser)
+    args1 = trainparser.parse_args()
+    data_transforms = transforms.Compose([
+        transforms.Pad((7, 0)),
+        transforms.Resize((128, 160)),
+        transforms.ToTensor(),
+    ])
+    roadmap_transforms = transforms.Compose([
+        transforms.Resize((400, 400)),
+        transforms.ToTensor()
+    ])
+    labeled_trainset = UnLabeledPlusLabeledDatasetScene(image_folder=image_folder,
+                                           annotation_file=annotation_csv,
+                                           unlabeled_scene_index=unlabeled_scene_index,
+                                           labeled_scene_index=labeled_scene_index,
+                                           transform=data_transforms,
+                                           roadmap_transform=roadmap_transforms,
+                                           extra_info=False,
+                                           scene_batch_size=1
+                                           )
+    trainset, testset = torch.utils.data.random_split(labeled_trainset, [int(0.90 * len(labeled_trainset)),
+                                                                         len(labeled_trainset) - int(
+                                                                             0.90 * len(labeled_trainset))])
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=args1.batch_size, shuffle=True, num_workers=1,
+                                              collate_fn=collate_fn_lstm)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=args1.batch_size, shuffle=False, num_workers=1,
+                                             collate_fn=collate_fn_lstm)
+
+    model = bothModel.trainModel(args1)
+    print("Model has {} paramerters in total".format(sum(x.numel() for x in model.parameters())))
+    trainer = pl.Trainer.from_argparse_args(args1)
+    trainer.fit(model, train_dataloader=trainloader, val_dataloaders=testloader)
